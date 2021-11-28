fbcd3748335b92ec33f053d8657d1cd08f49e45f
Peter Lamson <ptrlmsn@gmail.com>
2021-11-28 13:09:03 -0500

    commit tree e2097975e595bea3a8e5e37c4430be50e631c2e8

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React,{ useState } from "react";
 
 function Item({ name, category }) {
+  const [cartStatus, setCartStatus] = useState (false)
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={cartStatus ? 'in-cart' : ''} onClick={()=>{setCartStatus((cartStatus => !cartStatus))}}>{cartStatus ?"Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
